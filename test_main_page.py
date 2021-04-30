86c129402df8da33ebf9c936a540346dbc37b5aa
yu <inter_worker@mail.ru>
2021-04-30 12:42:59 +0300

    should_be_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -10,9 +10,9 @@ import os
 import pytest
 from .pages.main_page import MainPage
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+#def go_to_login_page(browser):
+#    login_link = browser.find_element_by_css_selector("#login_link")
+#    login_link.click()
     
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -21,6 +21,12 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор  экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()    
     
 if __name__ == "__main__":
     test_guest_can_go_to_login_page(browser)
